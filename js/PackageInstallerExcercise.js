6a68a88e2a673852eabcf5b22e6ba13a766ed4f0
milestyle <milesenglish88@gmail.com>
2017-06-12 13:55:17 -0600

    isDependent
    Created a function to check if one package is dependent on another.
    Created test to make sure it works.

--- js/PackageInstallerExcercise.js
+++ js/PackageInstallerExcercise.js
@@ -3,3 +3,8 @@ function getPackageOrder(packageList){
 	
 	return "hello";
 }
+
+
+function isDependent(item1, item2){
+	return (item1.split(": ")[0] == item2.split(": ")[1]);
+}
--- js/tests.js
+++ js/tests.js
@@ -2,3 +2,10 @@
 QUnit.test( "functoin getPackageOrder returns not null", function( assert ) {
   assert.ok( getPackageOrder("") != null, "Passed!" );
 });
+
+QUnit.test( "function isDependent returns true if dependent", function( assert ) {
+	item1 = "CamelCaser: ";
+	item2 = "KittenService: CamelCaser";
+  assert.ok( isDependent(item1, item2), "Passed!" );
+  assert.ok( !isDependent(item2, item1), "Passed!" );
+});
