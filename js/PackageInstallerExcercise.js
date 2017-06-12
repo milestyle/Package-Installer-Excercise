020e6f984d48538229bcaec52dc70746a92fceb0
milestyle <milesenglish88@gmail.com>
2017-06-12 14:46:54 -0600

    Implementing algorithm
    Used a insertion sort function to put them in an acceptable install
    order. Created function to return the package name for output. Created
    function that checks for loop. Basically since we know the sort works,
    we know that any item that's not "sorted" (it's listed after its
    dependent) must necessarily be part of a loop.

--- js/PackageInstallerExcercise.js
+++ js/PackageInstallerExcercise.js
@@ -1,10 +1,49 @@
 
 function getPackageOrder(packageList){
-	
-	return "hello";
+	try{
+		var sortedPackages = insertionSort(packageList);
+		if (checkForLoop(sortedPackages))
+			return "Error: Input must not contain a cycle";
+		var resultString = "";
+		for (var i = 0; i < sortedPackages.length; i++){
+			resultString += getPackageName(sortedPackages[i]);
+			if (i < sortedPackages.length - 1) resultString += ", ";
+		}
+		console.log(resultString);
+		return resultString;
+	}
+	catch(error){
+		console.log(error)
+		return error.message;
+	}
 }
 
+function insertionSort(items) {
+    var len     = items.length,value,i,j;
+    for (i=0; i < len; i++) {
+        value = items[i];
+        for (j=i-1; j > -1 && !isDependent(items[j], value); j--) {
+            items[j+1] = items[j];
+        }
+        items[j+1] = value;
+    }
+    return items;
+}
 
 function isDependent(item1, item2){
 	return (item1.split(": ")[0] == item2.split(": ")[1]);
+}
+
+function getPackageName(item){
+	return item.split(": ")[0];
+}
+
+function checkForLoop(sortedPackages){
+	for (var i = 0; i < sortedPackages.length; i++){
+		for (var j = 0; j < i; j++){
+			if (isDependent(sortedPackages[i], sortedPackages[j]))
+				return true;
+		}
+	}
+	return false;
 }
--- js/tests.js
+++ js/tests.js
@@ -1,11 +1,27 @@
 
-QUnit.test( "functoin getPackageOrder returns not null", function( assert ) {
+QUnit.test( "function getPackageOrder does not return null", function( assert ) {
   assert.ok( getPackageOrder("") != null, "Passed!" );
 });
 
-QUnit.test( "function isDependent returns true if dependent", function( assert ) {
+QUnit.test( "function isDependent returns true if dependent, false if not", function( assert ) {
 	item1 = "CamelCaser: ";
 	item2 = "KittenService: CamelCaser";
-  assert.ok( isDependent(item1, item2), "Passed!" );
-  assert.ok( !isDependent(item2, item1), "Passed!" );
+  assert.ok( isDependent(item1, item2), "succeeds if dependent" );
+  assert.ok( !isDependent(item2, item1), "succeeds if not dependent" );
+});
+
+QUnit.test( "function getPackageName returns the package name", function (assert) {
+	assert.ok(getPackageName("CamelCaser: Shadenfreude") == "CamelCaser", "Passed!");
+});
+
+QUnit.test( "algorithm works with sample data", function (assert) {
+	var excerciseInput = ["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "];
+	var expectedResult = "Ice, Cyberportal, Leetmeme, Fraudstream, KittenService, CamelCaser"
+	assert.ok(getPackageOrder(excerciseInput) == expectedResult, "Passed!");
+});
+
+QUnit.test( "algorithm detects cycles", function (assert) {
+	var loopedInput = ["KittenService: ","Leetmeme: Cyberportal","Cyberportal: Ice","CamelCaser: KittenService","Fraudstream: ","Ice: Leetmeme"];
+	var expectedResult = "Error: Input must not contain a cycle";
+	assert.ok(getPackageOrder(loopedInput) == expectedResult, "Passed!");
 });
